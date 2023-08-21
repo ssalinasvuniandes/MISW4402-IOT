bdefed4310e7c88c1998e4d7ef9fa2488746b99d
santiago alejandro salinas vargas <santiago.salinas@tatic.net>
2023-08-21 14:10:42 -0500

    se refactoriza el codigo apra darle orgen y facilitar su lectura

--- proyecto_iot/proyecto_iot.ino
+++ proyecto_iot/proyecto_iot.ino
@@ -1,13 +1,3 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
-// Ajustado por Santiago Salinas y Danuel Huerta
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-
 #include <ESP8266WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
@@ -17,105 +7,48 @@
 #include <PubSubClient.h>
 #include "secrets.h"
 
+// -----------------------------------------------------------------------------------------------
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor 
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-
-// Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
-DHT_Unified dht(DHTPIN, DHTTYPE);
-
-WiFiServer server(80);
-
-
-#define LED D1 // LED
-
-//Conexión a Wifi
-//Nombre de la red Wifi
-const char* ssid = "GOSA";
-//Contraseña de la red Wifi
-const char* password = "GOSA2022";
-
-//Conexión a Mosquitto
-//Usuario uniandes sin @uniandes.edu.co
+#define DHTPIN 2
+#define DHTTYPE DHT11
 #define HOSTNAME "s.salinasv"
+
+// -----------------------------------------------------------------------------------------------
+
+const int ANALOG_READ_PIN = A0;
+const char* ssid = "GOSA";
+const char* password = "GOSA2022";
 const char MQTT_HOST[] = "iotlab.virtual.uniandes.edu.co";
 const int MQTT_PORT = 8082;
-//Usuario uniandes sin @uniandes.edu.co
 const char MQTT_USER[] = "s.salinasv";
-//Contraseña de MQTT que recibió por correo
 const char MQTT_PASS[] = "202114041";
 const char MQTT_SUB_TOPIC[] = HOSTNAME "/";
-//Tópico al que se enviarán los datos de humedad
 const char MQTT_PUB_TOPIC1[] = "humedad/san_jose_del_guaviare/" HOSTNAME;
-//Tópico al que se enviarán los datos de temperatura
 const char MQTT_PUB_TOPIC2[] = "temperatura/san_jose_del_guaviare/" HOSTNAME;
+const char MQTT_PUB_TOPIC3[] = "luminosidad/san_jose_del_guaviare/" HOSTNAME;
+
+// -----------------------------------------------------------------------------------------------
 
 uint32_t delayMS;
 int ValueRead=2;
 int myflag=0;
-float Temperature;
-float Humidity;
-time_t now;
 unsigned long lastMillis = 0;
+sensor_t sensor;
+sensors_event_t event;
+time_t now;
 
-#if (defined(CHECK_PUB_KEY) and defined(CHECK_CA_ROOT)) or (defined(CHECK_PUB_KEY) and defined(CHECK_FINGERPRINT)) or (defined(CHECK_FINGERPRINT) and defined(CHECK_CA_ROOT)) or (defined(CHECK_PUB_KEY) and defined(CHECK_CA_ROOT) and defined(CHECK_FINGERPRINT))
-  #error "cant have both CHECK_CA_ROOT and CHECK_PUB_KEY enabled"
-#endif
+// -----------------------------------------------------------------------------------------------
 
+DHT_Unified dht(DHTPIN, DHTTYPE);
 BearSSL::WiFiClientSecure net;
 PubSubClient client(net);
 
+// -----------------------------------------------------------------------------------------------
 
-//Función que conecta el node a través del protocolo MQTT
-//Emplea los datos de usuario y contraseña definidos en MQTT_USER y MQTT_PASS para la conexión
-void mqtt_connect()
-{
-  //Intenta realizar la conexión indefinidamente hasta que lo logre
-  while (!client.connected()) {
-    Serial.print("Time: ");
-    Serial.print(ctime(&now));
-    Serial.print("MQTT connecting ... ");
-    if (client.connect(HOSTNAME, MQTT_USER, MQTT_PASS)) {
-      Serial.println("connected.");
-    } else {
-      Serial.println("Problema con la conexión, revise los valores de las constantes MQTT");
-      Serial.print("Código de error = ");
-      Serial.println(client.state());
-      if ( client.state() == MQTT_CONNECT_UNAUTHORIZED ) {
-        ESP.deepSleep(0);
-      }
-      /* Espera 5 segundos antes de volver a intentar */
-      delay(5000);
-    }
-  }
-}
-
-void receivedCallback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Received [");
-  Serial.print(topic);
-  Serial.print("]: ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-}
-
-
-
-void setup()
-{
-  Serial.begin(9600);
-  // Initialize device.
-  dht.begin();
+void setup_dht() {
+  // ... código para configurar dht ...
   Serial.println(F("DHT11 Unified Sensor"));
   // Print temperature sensor details.
-  sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   Serial.println(F("------------------------------------"));
   Serial.println(F("Temperature Sensor"));
@@ -138,36 +71,47 @@ void setup()
   Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
+}
 
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW); //LED comienza apagado
+// -----------------------------------------------------------------------------------------------
 
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
+void setup_wifi() {
+  // ... código para configurar wifi ...
+  Serial.print("Setting WiFi");
   WiFi.hostname(HOSTNAME);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+}
 
-  while (WiFi.status() != WL_CONNECTED) {
+// -----------------------------------------------------------------------------------------------
+
+void wifi_connect() {
+  // ... código para conectarse a wifi ...
+  Serial.print("WiFi Connect");
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Time: ");
+    Serial.print(ctime(&now));
+    Serial.print("WiFi connecting ... ");
+    WiFi.begin(ssid, password);
+
     if ( WiFi.status() == WL_NO_SSID_AVAIL || WiFi.status() == WL_WRONG_PASSWORD ) {
       Serial.print("\nProblema con la conexión, revise los valores de las constantes ssid y pass");
       ESP.deepSleep(0);
     } else if ( WiFi.status() == WL_CONNECT_FAILED ) {
       Serial.print("\nNo se ha logrado conectar con la red, resetee el node y vuelva a intentar");
       ESP.deepSleep(0);
+    } else if ( WiFi.status() == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("WiFi connected!");
     }
-    Serial.print(".");
-    delay(1000);
+  } else {
+    Serial.println("WiFi connected!");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+}
 
-  //Sincroniza la hora del dispositivo con el servidor SNTP (Simple Network Time Protocol)
-  Serial.println("");
+// -----------------------------------------------------------------------------------------------
+
+void setup_ntp() {
+  // ... código para configurar NTP ...
   Serial.print("Setting time using SNTP");
   configTime(-5 * 3600, 0, "pool.ntp.org", "time.nist.gov");
   now = time(nullptr);
@@ -176,14 +120,34 @@ void setup()
     Serial.print(".");
     now = time(nullptr);
   }
-  Serial.println("done!");
   struct tm timeinfo;
   gmtime_r(&now, &timeinfo);
   //Una vez obtiene la hora, imprime en el monitor el tiempo actual
   Serial.print("Current time: ");
   Serial.print(asctime(&timeinfo));
+  Serial.println("");
+  Serial.println("SNTP done!");
+}
 
+// -----------------------------------------------------------------------------------------------
 
+void receivedCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+}
+
+// -----------------------------------------------------------------------------------------------
+
+void setup_mqtt() {
+  // ... código para configurar mqtt ...
+  Serial.print("Setting MQTT");
+  #if (defined(CHECK_PUB_KEY) and defined(CHECK_CA_ROOT)) or (defined(CHECK_PUB_KEY) and defined(CHECK_FINGERPRINT)) or (defined(CHECK_FINGERPRINT) and defined(CHECK_CA_ROOT)) or (defined(CHECK_PUB_KEY) and defined(CHECK_CA_ROOT) and defined(CHECK_FINGERPRINT))
+    #error "cant have both CHECK_CA_ROOT and CHECK_PUB_KEY enabled"
+  #endif
   #ifdef CHECK_CA_ROOT
     BearSSL::X509List cert(digicert);
     net.setTrustAnchors(&cert);
@@ -202,86 +166,142 @@ void setup()
   //Llama a funciones de la librería PubSubClient para configurar la conexión con Mosquitto
   client.setServer(MQTT_HOST, MQTT_PORT);
   client.setCallback(receivedCallback);
-  //Llama a la función de este programa que realiza la conexión con Mosquitto
-  mqtt_connect();
-
 }
 
-//Función loop que se ejecuta indefinidamente repitiendo el código a su interior
-//Cada vez que se ejecuta toma nuevos datos de la lectura del sensor
-void loop()
-{
-  //Revisa que la conexión Wifi y MQTT siga activa
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print("Checking wifi");
-    while (WiFi.waitForConnectResult() != WL_CONNECTED)
-    {
-      WiFi.begin(ssid, password);
-      Serial.print(".");
-      delay(10);
+// -----------------------------------------------------------------------------------------------
+
+void mqtt_connect() {
+  Serial.println(F("MQTT Connect"));
+  if (!client.connected()) {
+    Serial.print("Time: ");
+    Serial.print(ctime(&now));
+    Serial.print("MQTT connecting ... ");
+
+    if (client.connect(HOSTNAME, MQTT_USER, MQTT_PASS)) {
+      Serial.println("connected.");
+    } else {
+      Serial.println("Connection failed");
+      Serial.println("State error: ");
+      Serial.println(client.state());
+      /* -4 : MQTT_CONNECTION_TIMEOUT - the server didn't respond within the keepalive time
+         -3 : MQTT_CONNECTION_LOST - the network connection was broken
+         -2 : MQTT_CONNECT_FAILED - the network connection failed
+         -1 : MQTT_DISCONNECTED - the client is disconnected cleanly
+         0 : MQTT_CONNECTED - the client is connected
+         1 : MQTT_CONNECT_BAD_PROTOCOL - the server doesn't support the requested version of MQTT
+         2 : MQTT_CONNECT_BAD_CLIENT_ID - the server rejected the client identifier
+         3 : MQTT_CONNECT_UNAVAILABLE - the server was unable to accept the connection
+         4 : MQTT_CONNECT_BAD_CREDENTIALS - the username/password were rejected
+         5 : MQTT_CONNECT_UNAUTHORIZED - the client was not authorized to connect
+      */
     }
-    Serial.println("connected");
   }
-  else
-  {
-    if (!client.connected())
-    {
+}
+
+// -----------------------------------------------------------------------------------------------
+
+void check_wifi_and_mqtt() {
+  // ... código para comprobar la conexión wifi y mqtt ...
+  connect_wifi();
+  if (WiFi.status() == WL_CONNECTED) && (!client.connected()){
       mqtt_connect();
-    }
-    else
-    {
-      client.loop();
-    }
   }
-  
+}
+
+// -----------------------------------------------------------------------------------------------
+
+float read_temperature() {
+  // ... código para leer la temperatura ...
   // Get temperature event and print its value.
-  sensors_event_t event;
+  float t;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
-    Temperature = -99;
-  }
-  else {
+    t = -99.0;
+  } else {
     Serial.print(F("Temperature: "));
-    Temperature = event.temperature;
-    Serial.print(Temperature);
+    t = event.temperature;
+    Serial.print(t);
     Serial.println(F("°C"));
   }
+  return t;
+}
 
+// -----------------------------------------------------------------------------------------------
+
+float read_humidity() {
+  // ... código para leer la humedad ...
   // Get humidity event and print its value.
+  float h;
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
-    Humidity = -99;
+    h = -99.0;
   }
   else {
     Serial.print(F("Humidity: "));
-    Humidity = event.relative_humidity;
+    h = event.relative_humidity;
     Serial.print(Humidity);
     Serial.println(F("%"));
   }
+}
 
-  now = time(nullptr);
+// -----------------------------------------------------------------------------------------------
+
+float read_luminosity() {
+  // ... código para leer la humedad ...
+  // obtien el valor del sesor de luz
+  int l = analogRead(ANALOG_READ_PIN);
+  // Values from 0-1024
+  Serial.print(F("Luminosidad (niveles): "));
+  Serial.println(l);
+  // Convert the analog reading to voltage
+  float voltage = l * (3.3 / 1023.0);
+  // print the voltage
+  Serial.print(F("Luminosidad (v): "));
+  Serial.println(voltage);
+  return voltage;
+}
+
+// -----------------------------------------------------------------------------------------------
+
+bool isValid(float value) {
+  // ... código para comprobar si un valor es válido ...
+  if (value==-99.0){
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// -----------------------------------------------------------------------------------------------
+
+void publish_data(float temperature, float humidity, float luminosity) {
+  // ... código para publicar datos ...
   //Transforma la información a la notación JSON para poder enviar los datos 
   //El mensaje que se envía es de la forma {"value": x}, donde x es el valor de temperatura o humedad
   
-  //JSON para humedad
-  String json = "{\"value\": "+ String(Humidity) + "}";
-  char payload1[json.length()+1];
-  json.toCharArray(payload1,json.length()+1);
   //JSON para temperatura
-  json = "{\"value\": "+ String(Temperature) + "}";
-  char payload2[json.length()+1];
-  json.toCharArray(payload2,json.length()+1);
+  String json = "{\"value\": "+ String(temperature) + "}";
+  char payload1[json.length()+1];
+  json.toCharArray(payload1, json.length()+1);
 
-  //Si los valores recolectados no son indefinidos, se envían a los tópicos correspondientes
-  if ( Humidity!=-99 && Temperature!=-99 ) {
-    //Publica en el tópico de la humedad
-    client.publish(MQTT_PUB_TOPIC1, payload1, false);
-    //Publica en el tópico de la temperatura
-    client.publish(MQTT_PUB_TOPIC2, payload2, false);
-  }
+  //JSON para humedad
+  json = "{\"value\": "+ String(humidity) + "}";
+  char payload2[json.length()+1];
+  json.toCharArray(payload2, json.length()+1);
+
+  //JSON para luz
+  json = "{\"value\": "+ String(luminosity) + "}";
+  char payload3[json.length()+1];
+  json.toCharArray(payload3, json.length()+1);
+
+  //Publica en el tópico de la temperatura
+  client.publish(MQTT_PUB_TOPIC1, payload1, false);
+  //Publica en el tópico de la humedad
+  client.publish(MQTT_PUB_TOPIC2, payload2, false);
+  //Publica en el tópico de la luz
+  client.publish(MQTT_PUB_TOPIC3, payload3, false);
 
   //Imprime en el monitor serial la información recolectada
   Serial.print(MQTT_PUB_TOPIC1);
@@ -290,8 +310,39 @@ void loop()
   Serial.print(MQTT_PUB_TOPIC2);
   Serial.print(" -> ");
   Serial.println(payload2);
-
-  // Delay between measurements.
-  delay(delayMS);
+  Serial.print(MQTT_PUB_TOPIC3);
+  Serial.print(" -> ");
+  Serial.println(payload3);
 
 }
+
+// -----------------------------------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  setup_dht();
+  setup_wifi();
+  wifi_connect();
+  setup_ntp();
+  setup_mqtt();
+  mqtt_connect();
+}
+
+// -----------------------------------------------------------------------------------------------
+
+void loop() {
+  check_wifi_and_mqtt();
+  
+  float temperature = read_temperature();
+  float humidity = read_humidity();
+  float luminosity = read_luminosity();
+
+  if (isValid(temperature) && isValid(humidity)) {
+    publish_data(temperature, humidity, luminosity);
+  }
+
+  delay(delayMS);
+}
+
+// -----------------------------------------------------------------------------------------------
