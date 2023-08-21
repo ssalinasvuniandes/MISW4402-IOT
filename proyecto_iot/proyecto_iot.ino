34cd7fdef47029d38d828dd910394d2f5da400cb
santiago alejandro salinas vargas <santiago.salinas@tatic.net>
2023-08-20 22:51:01 -0500

    version actualizada con conexión wifi y pagina web con datos sensados

--- proyecto_iot/proyecto_iot.ino
+++ proyecto_iot/proyecto_iot.ino
@@ -8,6 +8,7 @@
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
+#include <ESP8266WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
@@ -23,14 +24,20 @@
 
 // See guide for details on sensor wiring and usage:
 //   https://learn.adafruit.com/dht/overview
-
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-uint32_t delayMS;
+WiFiServer server(80);
+
 
 #define LED D1 // LED
+
+uint32_t delayMS;
 int ValueRead=2;
 int myflag=0;
+const char* ssid = "GOSA";
+const char* password = "GOSA2022";
+float Temperature;
+float Humidity;
 
 void setup()
 {
@@ -65,48 +72,125 @@ void setup()
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW); //LED comienza apagado
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
 }
 
 void loop()
 {
-  if (Serial.available()){
-    ValueRead=Serial.parseInt();
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
   }
 
-  if (((ValueRead==1 && myflag==0)|| myflag==1)&!(ValueRead==2 && myflag==1)){
-    digitalWrite(LED, HIGH);  // Se prende el LED
-    Serial.println("Prendido");
-    myflag=1;
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
   }
-  else{
-    digitalWrite(LED, LOW);   // Se apaga el LED
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Match the request
+
+  int value = HIGH;
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(LED, HIGH);
+    Serial.println("Prendido");
+    value = HIGH;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(LED, LOW);
     Serial.println("Apagado");
-    myflag=0;
+    value = LOW;
   }
   
-  // Delay between measurements.
-  delay(delayMS);
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  
+  client.print("LED is now: ");
+  
+  if(value == LOW) {
+  client.print("Off");
+  } else {
+  client.print("On");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("</html>");
+
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
+    Temperature = -99;
   }
   else {
     Serial.print(F("Temperature: "));
-    float t = event.temperature;
-    Serial.print(t);
+    Temperature = event.temperature;
+    Serial.print(Temperature);
     Serial.println(F("°C"));
   }
+
+  client.println("<br><br>");
+  client.print("Temperature: ");
+  client.print(Temperature);
+  client.print("C");
+
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
+    Humidity = -99;
   }
   else {
     Serial.print(F("Humidity: "));
-    float h = event.relative_humidity;
-    Serial.print(h);
+    Humidity = event.relative_humidity;
+    Serial.print(Humidity);
     Serial.println(F("%"));
   }
+
+  client.println("<br><br>");
+  client.print("Humidity: ");
+  client.print(Humidity);
+  client.print("%");
+
+  delay(1);
+  Serial.println("Client disconnected");
+  Serial.println("");
+ 
 }
